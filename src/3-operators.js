2febda55fb4795d6750a6b607f88e42b3bb550c0
unknown <SoSoBee>
2022-04-27 21:01:01 +0900

    3번 JS 파일

---
+++ src/3-operators.js
@@ -0,0 +1,16 @@
+// operators
+
+let x = 11;
+let y = 2;
+
+let result0 = x + y;
+let result1 = x * y;
+let result2 = x - y;
+let result3 = x / y;
+let result4 = x % y;
+
+console.log(result0);
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
