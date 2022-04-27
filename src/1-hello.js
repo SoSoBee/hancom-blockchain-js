505eebf571a882cf38d3d670e0a05d200d284a8e
unknown <SoSoBee>
2022-04-27 21:06:26 +0900

    모든 파일 업로드

---
+++ src/1-hello.js
@@ -0,0 +1,10 @@
+console.log("Hello 한컴");
+
+console.log("시험 시팔 개같은 거 왜 수업 못 듣게 해");
+
+console.log("codesandbox 좋은데 이거");
+
+console.log("single quote");
+console.log("double quote");
+
+console.log(`BACKTICK`);
---
+++ src/2-variables_constant.js
@@ -0,0 +1,31 @@
+// const -> constant
+
+const name = "var";
+
+// console.log(name);
+
+console.log(`저의 이름은 ${name} 입니다. `);
+
+console.log("자바스크립트에서는 이제 무조건 let 또는 const로 변수를 씁니다.");
+
+const myName = "var 1";
+console.log(myName);
+
+let age = 30;
+
+console.log(`2021년 저의 나이는 ${age} 입니다.`);
+
+age = 31;
+
+console.log(`2022년 저의 나이는 ${age} 입니다.`);
+
+// Full name 변수 짓기
+
+const full_name = "홍길동";
+console.log(full_name);
+
+let full_name2;
+console.log("full_name2 => ", full_name2);
+
+full_name2 = "홍길동";
+console.log("full_name2 => ", full_name2);
